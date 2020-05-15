0aeb0ee7618e0e20e24c57ef30b5aa1bc4155a69
Jonathan Cote <jonathancote92@gmail.com>
2020-05-15 14:01:53 -0500

    color fix

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import * as THREE from "three";
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00f00 } );
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
     var cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
     camera.position.z = 5;
